933cdf6500e3c20063dad4bd889e6cadda594f32
Pengdwende Abdoul GUira <guimani@yahoo.fr>
2022-01-27 18:18:09 -0500

    connedted to heroku

--- database/database.js
+++ database/database.js
@@ -1,14 +1,27 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize(
-    {
-        dialect: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password:'aA60120012',
-        database: 'movieworld'
+// const db = new Sequelize(
+//     {
+//         dialect: 'postgres',
+//         host: 'localhost',
+//         port: 5432,
+//         username: 'postgres',
+//         password:'aA60120012',
+//         database: 'movieworld'
+//     }
+
+
+// )
+
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-)
+  }
+);
+
 
 module.exports = db
